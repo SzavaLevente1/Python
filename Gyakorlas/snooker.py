6cd5e6aa15e5f4351fac4f2489373c7170614a3f
SzavaLevente1 <150236137+SzavaLevente1@users.noreply.github.com>
2023-12-06 18:23:53 +0100

    Add files via upload

---
+++ Gyakorlas/snooker.py
@@ -0,0 +1,18 @@
+snooker= []
+
+with open('snooker.txt','r',encoding='utf-8') as fajl:
+ for sor in fajl:
+    sor.split
+    snooker.append(sor)
+ print(snooker)
+
+print('3. feladat')
+print(f'A világranglistány {len(snooker[1:])} szerepel')
+
+print('4.feladat')
+
+print('5.feladat')
+
+print('6.feladat')
+
+print('7.feladat')
